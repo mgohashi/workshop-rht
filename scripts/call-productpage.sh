0070ab50f72504670349144e5f9761f90a889b0d
Marcelo Ohashi <mohashi@redhat.com>
2022-05-06 11:40:06 -0300

    Moving scripts

--- scripts/call-productpage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while true; do 
-    echo call-$(date | tr -d ' ')
-    curl -sk http://istio-ingressgateway-istio-system.apps.cluster-dfkdw.dfkdw.sandbox1648.opentlc.com/productpage > /dev/null
-    sleep 0.5
-done
