771981af6f5ea67ce71d6f0e2de881adc41f94b2
Marcelo Ohashi <mohashi@redhat.com>
2022-05-06 11:39:11 -0300

    Updating

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 oc apply -k $(dirname $0)/../pre-req/istio-system-ns/
-oc apply -f $(dirname $0)/openshift-gitops-ns-application.yml
+oc apply -f $(dirname $0)/../pre-req/openshift-gitops-ns/openshift-gitops-ns-application.yml
