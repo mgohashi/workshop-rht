6d4f96b9a1dd4dd336eebf24fb460f7ff1b84c9a
Marcelo Ohashi <mohashi@redhat.com>
2022-05-18 08:00:51 -0300

    adding kustomization

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-oc apply -k $(dirname $0)/../pre-req/istio-system-ns/
-oc apply -f $(dirname $0)/../pre-req/openshift-gitops-ns/openshift-gitops-ns-application.yml
+oc apply -k $(dirname $0)/../pre-req/istio-system-ns/ -n istio-system
+oc apply -f $(dirname $0)/../pre-req/istio-system-ns/jaeger.yml -n istio-system
+# sleep 200
+oc apply -f $(dirname $0)/../pre-req/istio-system-ns/basic-smcp.yml -n istio-system
+oc apply -f $(dirname $0)/../pre-req/openshift-gitops-ns/openshift-gitops-ns-application.yml -n openshift-gitops
